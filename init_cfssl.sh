7644576677052e1f153fe865947976520976550e
Phil Cadell <phil.cadell@itoc.com.au>
2020-10-30 15:35:28 +1000

    Change default config to 90d certificate expiration.

--- init_cfssl.sh
+++ init_cfssl.sh
@@ -5,4 +5,4 @@ aws ssm get-parameter --name bpm-ca-intermediate-private-key --region ap-southea
 aws ssm get-parameter --name bpm-ca-intermediate-public-key --region ap-southeast-2 --with-decryption --output text --query Parameter.Value > /etc/cfssl/intermediate_ca.pem && \
 
 # Serve CFSSL
-cfssl serve -address=0.0.0.0 -ca-key=/etc/cfssl/intermediate_ca-key.pem -ca=/etc/cfssl/intermediate_ca.pem
+cfssl serve -address=0.0.0.0 -ca-key=/etc/cfssl/intermediate_ca-key.pem -ca=/etc/cfssl/intermediate_ca.pem -config=./config/config.json
